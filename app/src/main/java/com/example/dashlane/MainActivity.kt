a5d57de9cf75edd591c4b1c4d116e51fac820894
Gaddar Kr Chaudhary <gaddarkumar7447@gmail.com>
2023-03-21 17:15:33 +0530

    Adding my own library to check wheather its working or not

--- app/src/main/java/com/example/dashlane/MainActivity.kt
+++ app/src/main/java/com/example/dashlane/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.dashlane
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dashlane.databinding.ActivityMainBinding
+import com.example.firstlibrarycreated.DesignToast
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        DesignToast.smartToast(this, "Gaddar Kumar Chaudhary")
     }
 }
