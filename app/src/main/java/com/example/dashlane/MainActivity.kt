4152a76eafcb1e5323f934a6de90754b0de7e691
Gaddar Kr Chaudhary <gaddarkumar7447@gmail.com>
2023-04-14 18:03:28 +0530

    Update UI

--- app/src/main/java/com/example/dashlane/MainActivity.kt
+++ app/src/main/java/com/example/dashlane/MainActivity.kt
@@ -2,16 +2,35 @@ package com.example.dashlane
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.dashlane.databinding.ActivityMainBinding
-import com.example.firstlibrarycreated.DesignToast
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
     private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        supportActionBar?.hide()
 
-        DesignToast.smartToast(this, "Gaddar Kumar Chaudhary")
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHostFragment
+        navController = navHostFragment.navController
+        val bottomNavigationView = binding.bottomNav
+        bottomNavigationView.setupWithNavController(navController)
+
+       // setupFragments()
+    }
+
+    private fun setupFragments() {
+        val bottomNavigationView = binding.bottomNav
+        val navController : NavController = findNavController(R.id.fragmentContainer)
+        //val appBarConfiguration = AppBarConfiguration(setOf(R.id.homeFragment, R.id.cardFragment, R.id.passwordFragment, R.id.settingsFragment))
+        //setupActionBarWithNavController(navController, appBarConfiguration)
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
